c36e3217302f822af5f15c8192ea11046b97f273
ammasum <abdullamuhammed76@gmail.com>
2020-04-15 23:51:39 +0600

    server Muliple ip

--- websocker_server.js
+++ websocker_server.js
@@ -1,5 +1,7 @@
 const net = require('net');
 const Events = require('events');
+const os = require('os');
+const ifaces = os.networkInterfaces();
 const connectionHandler = require('./connection_handler');
 module.exports = class {
     server;
@@ -36,6 +38,28 @@ module.exports = class {
         })
         this.server.listen(this.port, this.host, () => {
             console.log(`Server started at http://${this.host}:${this.port}`);
+        });
+
+        Object.keys(ifaces).forEach((ifname) => {
+            var alias = 0;
+
+            ifaces[ifname].forEach((iface) => {
+                if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+                }
+
+                if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+                } else {
+                // this interface has only one ipv4 adress
+                    this.server.listen(this.port, iface.address, () => {
+                        console.log(`Server started at http://${iface.address}:${this.port}`);
+                    });
+                }
+                ++alias;
+            });
         });
     }
     
